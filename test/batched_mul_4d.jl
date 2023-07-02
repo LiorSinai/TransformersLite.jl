512f1a8832479970c264343b96e4d89548fdcacc
Lior Sinai <sinailior@gmail.com>
2023-07-02 22:57:45 +0200

    Test errors for batched mul 4d

--- test/batched_mul_4d.jl
+++ test/batched_mul_4d.jl
@@ -3,6 +3,11 @@ using TransformersLite: batched_mul
 using Flux: pullback
 
 @testset "batched_mul 4d" begin
+    # Integer
+    A = rand(-100:100, 2, 2, 3, 4)
+    B = rand(-100:100, 2, 2, 3, 4)
+    @test multiply_test(A, B, batched_mul(A, B))
+
     # Real
     A = randn(7, 5, 3, 4)
     B = randn(5, 7, 3, 4)
@@ -14,11 +19,15 @@ using Flux: pullback
     @test multiply_test(AT, BT, batched_mul(AT, BT))
     @test multiply_test(AT, C, batched_mul(AT, C))
     @test multiply_test(AT, A, batched_mul(AT, A))
-
-    # Integer
-    A = rand(-100:100, 2, 2, 3, 4)
-    B = rand(-100:100, 2, 2, 3, 4)
-    @test multiply_test(A, B, batched_mul(A, B))
+    
+    # Errors
+    A = randn(7, 5, 3, 4)
+    B = randn(4, 7, 3, 4)
+    @test_throws DimensionMismatch batched_mul(A, B)
+    B = randn(5, 7, 2, 4)
+    @test_throws DimensionMismatch batched_mul(A, B)
+    B = randn(5, 7, 3, 2)
+    @test_throws DimensionMismatch batched_mul(A, B)
 end
 
 @testset "batched_mul 4d grad" begin
--- test/mul4d.jl
+++ test/mul4d.jl
@@ -3,6 +3,11 @@ using TransformersLite: mul4d
 using Flux: pullback
 
 @testset "mul4d" begin
+    # Integer
+    A = rand(-100:100, 2, 2, 3, 4)
+    B = rand(-100:100, 2, 2, 3, 4)
+    @test multiply_test(A, B, mul4d(A, B))
+
     # Real
     A = randn(7, 5, 3, 4)
     B = randn(5, 7, 3, 4)
@@ -15,10 +20,14 @@ using Flux: pullback
     @test multiply_test(AT, C, mul4d(AT, C))
     @test multiply_test(AT, A, mul4d(AT, A))
 
-    # Integer
-    A = rand(-100:100, 2, 2, 3, 4)
-    B = rand(-100:100, 2, 2, 3, 4)
-    @test multiply_test(A, B, mul4d(A, B))
+    # Errors
+    A = randn(7, 5, 3, 4)
+    B = randn(4, 7, 3, 4)
+    @test_throws DimensionMismatch mul4d(A, B)
+    B = randn(5, 7, 2, 4)
+    @test_throws DimensionMismatch mul4d(A, B)
+    B = randn(5, 7, 3, 2)
+    @test_throws DimensionMismatch mul4d(A, B)
 end
 
 @testset "mul4d grad" begin
